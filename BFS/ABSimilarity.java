eaaa9b86a59b9ac0ea1af3af3fbefb4eb2e2612b
Leecode <53207016+guodalongplus@users.noreply.github.com>
2020-05-15 23:08:55 +0800

    Add files via upload

---
+++ BFS/ABSimilarity.java
@@ -0,0 +1,61 @@
+package com.leecode.BFS;
+
+import java.util.*;
+
+public class ABSimilarity {
+    public int kSimilarity(String A, String B) {
+        Queue<String> queue = new ArrayDeque();
+        queue.offer(A);
+
+        Map<String, Integer> dist = new HashMap();
+        dist.put(A, 0);
+
+        while (!queue.isEmpty()) {
+            String S = queue.poll();
+            if (S.equals(B)) return dist.get(S);
+            for (String T: neighbors(S, B)) {
+                if (!dist.containsKey(T)) {
+                    dist.put(T, dist.get(S) + 1);
+                    queue.offer(T);
+                }
+            }
+        }
+
+        throw null;
+    }
+
+    public List<String> neighbors(String S, String target) {
+        List<String> ans = new ArrayList();
+        int i = 0;
+        for (; i < S.length(); ++i) {
+            if (S.charAt(i) != target.charAt(i)) break;
+        }
+
+        char[] T = S.toCharArray();
+        for (int j = i+1; j < S.length(); ++j)
+            if (S.charAt(j) == target.charAt(i)) {
+                swap(T, i, j);
+                ans.add(new String(T));
+                swap(T, i, j);
+            }
+
+        return ans;
+    }
+
+    public void swap(char[] T, int i, int j) {
+        char tmp = T[i];
+        T[i] = T[j];
+        T[j] = tmp;
+    }
+
+
+    public static void main(String[] args) {
+        ABSimilarity code=new ABSimilarity();
+//        String A="abccaacceecdeea";
+//        String B="bcaacceeccdeaae";
+        String A="abcd";
+        String B="bdac";
+
+        System.out.println(code.kSimilarity(A,B));
+    }
+}
