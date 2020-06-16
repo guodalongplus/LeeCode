04d5207b2b92d9222ce510b6be55d29a180babe3
Leecode <53207016+guodalongplus@users.noreply.github.com>
2020-06-16 17:06:19 +0800

    Update MakeQueue.java

--- Stack/MakeQueue.java
+++ Stack/MakeQueue.java
@@ -1,7 +1,10 @@
 package com.leecode.Stack;
 
 import java.util.Stack;
-
+/*
+* 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+* 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+*/
 class MakeQueue {
     Stack<Integer> stack1;
     Stack<Integer> stack2;
