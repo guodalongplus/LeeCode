20f8944df305b534327e96b02a3d2799ecd0a20a
Leecode <53207016+guodalongplus@users.noreply.github.com>
2020-06-24 18:22:02 +0800

    Add files via upload

---
+++ Point/FindDuplicate.java
@@ -0,0 +1,34 @@
+package com.leecode.point;
+/*
+*287. 寻找重复数
+给定一个包含 n + 1 个整数的数组 nums，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。假设只有一个重复的整数，找出这个重复的数。
+* */
+public class FindDuplicate {
+    public static int findDuplicate(int[] nums) {
+        int n = nums.length;
+        int l = 1, r = n - 1, ans = -1;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            //cnt记录的是小于当前mid的元素个数
+            int cnt = 0;
+            for (int i = 0; i < n; ++i) {
+                if (nums[i] <= mid) {
+                    cnt++;
+                }
+            }
+            if (cnt <= mid) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+                ans = mid;
+            }
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        int[]nums={1,3,4,2,2,2};
+        System.out.println(findDuplicate(nums));
+    }
+}
