f87493d1aa1a3e90eb6920c1c3224e0f8e63e1db
Leecode <53207016+guodalongplus@users.noreply.github.com>
2020-08-10 23:52:56 +0800

    Add files via upload
    二进制加法

---
+++ String/AddBinary.java
@@ -0,0 +1,32 @@
+package com.leecode.String;
+
+
+public class demo {
+        public static String addBinary(String a, String b) {
+            StringBuffer ans = new StringBuffer();
+
+            int n = Math.max(a.length(), b.length()), carry = 0;
+            for (int i = 0; i < n; ++i) {
+                carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+                carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+                ans.append((char) (carry % 2 + '0'));
+                carry /= 2;
+            }
+
+            if (carry > 0) {
+                ans.append('1');
+            }
+            ans.reverse();
+
+            return ans.toString();
+        }
+
+    public static void main(String[] args) {
+        String a="1010011";
+        String b="11";
+
+
+        System.out.println(addBinary(a,b));
+    }
+
+}
