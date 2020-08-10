84d8e05e8954aa2bb35282ab6ece2ce442eedd40
Leecode <53207016+guodalongplus@users.noreply.github.com>
2020-08-10 23:54:18 +0800

    二进制求和

---
+++ String/AddBinary.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <math.h>
+#include<string.h>
+void reserve(char* s) {
+    int len = strlen(s);
+    for (int i = 0; i < len / 2; i++) {
+        char t = s[i];
+        s[i] = s[len - i - 1], s[len - i - 1] = t;
+    }
+}
+
+char* addBinary(char* a, char* b) {
+    reserve(a);
+    reserve(b);
+
+    int len_a = strlen(a), len_b = strlen(b);
+    int n = fmax(len_a, len_b), carry = 0, len = 0;
+    char* ans = (char*)malloc(sizeof(char) * (n + 2));
+    for (int i = 0; i < n; ++i) {
+        carry += i < len_a ? (a[i] == '1') : 0;
+        carry += i < len_b ? (b[i] == '1') : 0;
+        ans[len++] = carry % 2 + '0';
+        carry /= 2;
+    }
+
+    if (carry) {
+        ans[len++] = '1';
+    }
+    ans[len] = '\0';
+    reserve(ans);
+
+    return ans;
+}
+
+
+int  main(){
+	char s1[] ="110";
+	char s2[] ="10";
+	char* arr=addBinary(s1,s2);
+	printf("%s",arr);
+
+} 
